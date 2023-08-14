a9e96e4a98fea380fbd1163df002504625576847
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-08-14 06:27:15 +0000

    progate　レッスン1中断

---
+++ progate.java
@@ -0,0 +1,32 @@
+public class progate {
+
+  public static void main(String []args) {
+    System.out.println("Hello, World!");
+    System.out.println(3);
+    System.out.println(5 + 2);
+    System.out.println(8 - 5);
+    System.out.println("5 + 2");
+    System.out.println(6 / 2);
+    System.out.println(8 % 5);
+    System.out.println("Ninja" + "Wanko");
+    System.out.println("5" + "3");
+
+    // データ型 変数名;
+    // int number;
+    // String name;
+
+    // 値の代入
+    // number = 3;
+
+    int number;
+    number = 10;
+    System.out.println(number);
+    System.out.println(number);
+
+    String name;
+    name = "Sato";
+    System.out.println(name);
+
+
+  }
+}
