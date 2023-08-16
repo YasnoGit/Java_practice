6670a86add689ecf951d4af5725f40a91fc61027
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-08-16 05:54:02 +0000

    レッスン1終了

--- progate.java
+++ progate.java
@@ -1,3 +1,4 @@
+// レッスン1
 public class progate {
 
   public static void main(String []args) {
@@ -21,12 +22,30 @@ public class progate {
     int number;
     number = 10;
     System.out.println(number);
-    System.out.println(number);
 
     String name;
     name = "Sato";
     System.out.println(name);
 
+    String lastName = "Sato";
 
+    double number = 5.28;
+    System.out.println(number);
+
+    int month = 12;
+    int date = 31;
+    // 12月31日
+    System.out.println(month + "月" + date + "日");
+    // 7を2で割った値
+    System.out.println(7 / 2);
+    // 7.0を2.0で割った値
+    System.out.println(7.0 /2.0);
+    // 7を2.0で割った値
+    System.out.println(7 /2.0);
+
+    // キャスト int→double
+    int number1 = 13;
+    int number2 = 4;
+    System.out.println((double)number1 / number2);
   }
 }
