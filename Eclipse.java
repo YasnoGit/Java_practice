31de9d489dba2677bd2978feefc82080f4b29894
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-09-14 08:38:42 +0000

    繰り返しアルゴリズム演習

--- Eclipse.java
+++ Eclipse.java
@@ -329,5 +329,95 @@ public class JavaSample {
 		}
 		System.out.print("奇数 : " + str1.substring(0, str1.length() - 1) + "\n");
 		System.out.print("偶数 : " + str0.substring(0, str0.length() - 1) + "\n");
+
+		//1〜10の数字の偶数のみを大きい順に上から縦に並ぶように表示させる
+		int w = 10;
+		while ( 1 <= w ) {
+		if ( 0 == ( w % 2 ) )
+			System.out.println( w );
+			w--;
+	  }
+
+    //1〜10までの偶数が何個あるのか、偶数の合計は幾つになるかを表示させる
+		int sum = 0;
+		int cnt = 0;
+		for (int j = 1; j <= 10; j++) {
+			if (j % 2 == 0) {
+				sum += j;
+				cnt++;
+			}
+		}
+		System.out.println("合計" + sum);
+		System.out.println("個数" + cnt);
+
+    //1〜10までの整数をカンマ区切りで横並びで表示（10にはカンマをつけない）させる
+		int[] number = {1,2,3,4,5,6,7,8,9,10};
+		for (int n = 0; n < number.length; n++) {
+			System.out.print(number[n]);
+			if (number[n] != 10)
+			System.out.print(",");
+		}
+
+    //要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
+		int num[] = new int[20];
+		num[0] = 0;
+		for(int i = 1; i < num.length; i++) {
+		   num[i] = num[i - 1] + 5;
+		}
+		String str0 ="";
+		String str1 ="";
+		for(int i = 0; i < num.length; i++) {
+		   int x = num[i];
+		   int mod = x % 2;
+		   str1 += i < 11 && mod == 1 ? x + "," : "";
+		   str0 += i > 10 && mod == 0 ? x + "," : "";
+		}
+		System.out.print("奇数 : " + str1.substring(0, str1.length() - 1) + "\n");
+		System.out.print("偶数 : " + str0.substring(0, str0.length() - 1) + "\n");
+
+		//1〜10の数字の偶数のみを大きい順に上から縦に並ぶように表示させる
+		int w = 10;
+		while ( 1 <= w ) {
+		if ( 0 == ( w % 2 ) )
+			System.out.println( w );
+			w--;
+	  }
+
+    //1〜10までの偶数が何個あるのか、偶数の合計は幾つになるかを表示させる
+		int sum = 0;
+		int cnt = 0;
+		for (int j = 1; j <= 10; j++) {
+			if (j % 2 == 0) {
+				sum += j;
+				cnt++;
+			}
+		}
+		System.out.println("合計" + sum);
+		System.out.println("個数" + cnt);
+
+    //1〜10までの整数をカンマ区切りで横並びで表示（10にはカンマをつけない）させる
+		int[] number = {1,2,3,4,5,6,7,8,9,10};
+		for (int n = 0; n < number.length; n++) {
+			System.out.print(number[n]);
+			if (number[n] != 10)
+			System.out.print(",");
+		}
+
+    //要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
+		int num[] = new int[20];
+		num[0] = 0;
+		for(int i = 1; i < num.length; i++) {
+		   num[i] = num[i - 1] + 5;
+		}
+		String str0 ="";
+		String str1 ="";
+		for(int i = 0; i < num.length; i++) {
+		   int x = num[i];
+		   int mod = x % 2;
+		   str1 += i < 11 && mod == 1 ? x + "," : "";
+		   str0 += i > 10 && mod == 0 ? x + "," : "";
+		}
+		System.out.print("奇数 : " + str1.substring(0, str1.length() - 1) + "\n");
+		System.out.print("偶数 : " + str0.substring(0, str0.length() - 1) + "\n");
 	}
 }
